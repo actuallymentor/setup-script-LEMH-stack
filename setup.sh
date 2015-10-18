4eec5e846a31a2c9bd3b726569b491b8448b0681
Mentor <mentor@palokaj.co>
2015-10-18 12:04:15 +0200

    Removed mysql_secure_installation

--- setup.sh
+++ setup.sh
@@ -255,7 +255,7 @@ echo "$gzip" > /usr/local/nginx/conf/gzip.conf;
 
 # Mariadb
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-sudo add-apt-repository  -y 'deb http://mirror.i3d.net/pub/mariadb/repo/10.1/ubuntu vivid main' && sudo apt-get update
+sudo add-apt-repository -y 'deb http://mirror.i3d.net/pub/mariadb/repo/10.1/ubuntu vivid main' && sudo apt-get update
 sudo apt-get install -y mariadb-server
 sudo service mysql start
 
